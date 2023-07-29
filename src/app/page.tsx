f7931f6281141a63dc9addb85299db03742ad318
Wil Macedo <wil.macedo.sa@gmail.com>
2023-07-29 12:02:58 -0300

    feat: :sparkles: add main texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 import { Navbar } from "@/components/Navbar";
+import { twMerge } from "tailwind-merge";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-black">
       <Navbar />
 
-      <div className="mt-24 mx-8 sm:mx-24 md:mx-32 lg:mx-52">
-        <span>ola</span>
+      <div
+        className={twMerge("mt-12 mx-8 sm:mx-24 md:mx-32 lg:mx-52", "sm:mt-24")}
+      >
+        <div className={twMerge("flex flex-col", "md:flex-row")}>
+          <div className={twMerge("flex flex-col gap-4")}>
+            <h3 className="text-lg">/ Take Controls on</h3>
+            <h1 className="text-8xl font-bold">Will Pay</h1>
+            <span className="mt-4 text-sm font-light text-white text-opacity-70">
+              Manage{" "}
+              <strong className="font-medium text-slate-200">
+                Bill and Subscriptions
+              </strong>{" "}
+              from one place and pay on schedule that works for you.
+            </span>
+          </div>
+
+          <span>Right side</span>
+        </div>
       </div>
     </div>
   );
