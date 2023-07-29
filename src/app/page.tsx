0d4be64d0dd89df4557cb465c09326eb46ae1c35
Wil Macedo <wil.macedo.sa@gmail.com>
2023-07-29 11:55:35 -0300

    feat: :zap: fix navbar responsivite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-black">
       <Navbar />
+
+      <div className="mt-24 mx-8 sm:mx-24 md:mx-32 lg:mx-52">
+        <span>ola</span>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,28 @@ import { Button } from "./Button";
 
 export function Navbar() {
   return (
-    <nav className="pt-6 flex mx-8 sm:mx-24 md:mx-32 lg:mx-52 items-center justify-between ">
-      <div className="flex items-center gap-2">
+    <nav
+      className={twMerge(
+        "pt-6 mx-2 flex flex-col",
+        "sm:mx-24 sm:flex-row sm:justify-between md:mx-32 lg:mx-52"
+      )}
+    >
+      <div className="mx-auto flex items-center gap-2 sm:mx-0">
         <div className="bg-white rounded-t-full w-8 h-4 rotate-90" />
         <span className={twMerge("text-xs cursor-pointer", "hover:underline")}>
           / wil.macedo.sa@gmail.com
         </span>
       </div>
-      <div className="flex items-center gap-2">
-        <Button>Log In</Button>
-        <Button color="primary">Get Started</Button>
+      <div
+        className={twMerge(
+          "mt-8 flex flex-col items-center gap-2",
+          "sm:mt-0 sm:flex-row"
+        )}
+      >
+        <Button className="w-full sm:w-auto">Log In</Button>
+        <Button className="w-full sm:w-auto" color="primary">
+          Get Started
+        </Button>
       </div>
     </nav>
   );
